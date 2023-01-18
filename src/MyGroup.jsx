bbec03b8e800ab70d450443f5951c1c37df4356b
Ali-consciousroad <ali_dindar@live.be>
2023-01-18 16:35:18 +0100

    Create a renderer

---
+++ src/MyGroup.jsx
@@ -0,0 +1,39 @@
+import { MaterialLabelableLayoutRendererProps } from '@jsonforms/material-renderers';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Hidden,
+  Typography,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import React from 'react';
+import { withJsonFormsLayoutProps } from '@jsonforms/react';
+
+const MyGroup = (props) => {
+  const { uischema, schema, path, visible, renderers } = props;
+
+  const layoutProps = {
+    elements: uischema.elements,
+    schema: schema,
+    path: path,
+    direction: 'column',
+    visible: visible,
+    uischema: uischema,
+    renderers: renderers,
+  };
+  return (
+    <Hidden xsUp={!visible}>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>{uischema.label}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <MaterialLabelableLayoutRenderer {...layoutProps} />
+        </AccordionDetails>
+      </Accordion>
+    </Hidden>
+  );
+};
+
+export default withJsonFormsLayoutProps(MyGroupRenderer);
